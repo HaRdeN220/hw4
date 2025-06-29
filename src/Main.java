141d7ecf1573738f15376032b29e22082f0a3d1c
egor <vlad.filin_2000@mail.ru>
2025-06-29 22:57:53 +0300

    hw

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,11 @@
 public class Main {
     public static void main(String[] args) {
         int clientOS = 1;
-        switch (clientOS) {
-            case 0:
-                System.out.println("Установите версию приложения для iOS по ссылке");
-                break;
-            case 1:
-                System.out.println("Установите версию приложения для Android по ссылке");
-                break;
-            default:
-                System.out.println("Вы ввели некорректную операционную систему");
+        if (clientOS == 1){
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
         }
         // дз 1
 
