1ddf9ccd5503bad91f5a4b42bed288248eff5efb
egor <vlad.filin_2000@mail.ru>
2025-06-29 22:59:32 +0300

    hw

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,9 @@
 public class Main {
     public static void main(String[] args) {
         int clientOS = 1;
-        if (clientOS == 1){
+        if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        }
-        else {
+        } else {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
         // дз 1
